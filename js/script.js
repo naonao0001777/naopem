ef44e91758895a9a01941bd8b629df8e68e3d51f
nao <abi_wase1060@yahoo.co.jp>
2021-07-15 10:27:59 +0900

    フェード院

--- js/script.js
+++ js/script.js
@@ -46,24 +46,66 @@ $(function () {
     return false;
   });
 
+  // ページトップの表示
   var showFlag = false;
-    var topBtn = $('#js-page-top');    
-    topBtn.css('bottom', '-100px');
-    var showFlag = false;
-    //スクロールが100に達したらボタン表示
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 300) {
-            if (showFlag == false) {
-                showFlag = true;
-                topBtn.stop().animate({'bottom' : '20px'}, 200); 
-            }
-        } else {
-            if (showFlag) {
-                showFlag = false;
-                topBtn.stop().animate({'bottom' : '-100px'}, 550); 
-            }
-        }
-      });
+  var topBtn = $('#js-page-top');
+  topBtn.css('bottom', '-100px');
+  var showFlag = false;
+  //スクロールが100に達したらボタン表示
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 300) {
+      if (showFlag == false) {
+        showFlag = true;
+        topBtn.stop().animate({ 'bottom': '20px' }, 200);
+      }
+    } else {
+      if (showFlag) {
+        showFlag = false;
+        topBtn.stop().animate({ 'bottom': '-100px' }, 550);
+      }
+    }
+  });
+  // 途中のところで更新をしても表示
+  $(window).on("load",function () {
+    if ($(this).scrollTop() > 300) {
+      if (showFlag == false) {
+        showFlag = true;
+        topBtn.stop().animate({ 'bottom': '20px' }, 200);
+      }
+    }
+  });
+
+  // // スクロール途中から表示したいメニューバーを指定
+  // var navBox = $(".gnav");
+  // // メニューバーは初期状態では消しておく
+  // navBox.hide();
+  // // 表示を開始するスクロール量を設定(px)
+  // var TargetPos = 350;
+  // // スクロールされた際に実行
+  // $(window).scroll(function () {
+  //   // 現在のスクロール位置を取得
+  //   var ScrollPos = $(window).scrollTop();
+  //   // 現在のスクロール位置と、目的のスクロール位置を比較
+  //   if (ScrollPos > TargetPos) {
+  //     // 表示(フェイドイン)
+  //     navBox.fadeIn();
+  //   }
+  //   else {
+  //     // 非表示(フェイドアウト)
+  //     navBox.fadeOut();
+  //   }
+  // });
+
+  // 画面ロード時フェードインする
+  $(window).on("load", function () {
+    // ナビゲーションバー
+    $(".gnav").fadeIn(500);
+
+    // タイトルロゴ
+    setTimeout(function () {
+      $(".logo").fadeIn(1300);
+    }, 500);
+  });
 
   // // マウスオーバーでかっこよくしようとしたよ
   // $('.skill-content').hover(function () {
